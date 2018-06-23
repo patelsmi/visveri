63113ce60f4f07627de44ae4862d03e612bf8d06
Smit Patel <patel.smi@husky.neu.edu>
2018-06-22 21:49:07 -0700

    Adding get functions in db class

--- source/read.py
+++ source/read.py
@@ -44,41 +44,53 @@ class database:
         self.last_module = submodule
         self.add_module(submodule)
 
-    def print_inputs(self, module=None):
+    def get_inputs(self, module=None):
         if module is None:
             if self.last_module is None:
                 hlp.error(custom_msg='No module added in database yet!', fatal=False)
-                return 1
+                return None
             else:
                 module = self.last_module
+        return self.design[module]['inputs']
+
+    def print_inputs(self, module=None):
+        all_inputs = self.get_inputs(module)
         print "Inputs of module: %s" %module
-        for port in self.design[module]['inputs']:
+        for port in all_inputs:
             print port
         hlp.print_new_line()
+
+    def get_outputs(self, module=None):
+        if module is None:
+            if self.last_module is None:
+                hlp.error(custom_msg='No module added in database yet!', fatal=False)
+                return None
+            else:
+                module = self.last_module
+        return self.design[module]['outputs']
 
     def print_outputs(self, module=None):
-        if module is None:
-            if self.last_module is None:
-                hlp.error(custom_msg='No module added in database yet!', fatal=False)
-                return 1
-            else:
-                module = self.last_module
+        all_outputs = self.get_outputs(module)
         print "Outputs of module: %s" %module
-        for port in self.design[module]['outputs']:
+        for port in all_outputs:
             print port
         hlp.print_new_line()
 
-    def print_submodules(self, module=None):
+    def get_submodules(self, module=None):
         if module is None:
             if self.last_module is None:
                 hlp.error(custom_msg='No module added in database yet!', fatal=False)
-                return 1
+                return None
             else:
                 module = self.last_module
+        return self.design[module]['submodules']
+
+    def print_submodules(self, module=None):
+        submodules = self.get_submodules(module)
         print "For module: %s" %module
         print " Instance   --->   Submodule"
-        for instance in self.design[module]['submodules'].keys():
-            print " %s   --->   %s " %(instance, self.design[module]['submodules'][instance])
+        for instance in submodules.keys():
+            print " %s   --->   %s " %(instance, submodules[instance])
         hlp.print_new_line()
 
 
